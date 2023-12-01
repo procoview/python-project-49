c4e9e76d6aa7bee9e46f913c6b3616b0c6b724a9
procoview <simon10-00@mail.ru>
2023-12-02 00:02:05 +0300

    update brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# file <brain_even.py>
+
+from engine import *
+import brain_games.games.game_calc
+
+
+def main():
+    launch_game(brain_games.games.game_calc)
+
+
+if __name__ == '__main__':
+    main()
