90ff7bca2fcde5106f08b7de7e22a6e8b191d8e9
procoview <simon10-00@mail.ru>
2023-12-01 22:56:46 +0300

    [H

---
+++ brain_games/engine.py
@@ -0,0 +1,26 @@
+# file <engine.py>
+
+def launch_game(game):
+    print("Welcome to the Brain Games!")
+    
+    name = input("May I have your name? ")
+    
+    print(f"Hello, {name}!")
+    
+    question = game.question
+    
+    print(question)
+    
+    i = 0
+    while i < 3:
+        game.main()
+        result = game.result
+        answer = input("Your answer: ")
+        if answer == result:
+            print("Correct!")
+            i += 1
+        else:
+            i = 0
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{result}'.")
+            print(f"Let's try again, {name}!")
+    print(f"Congratulations, {name}!")
