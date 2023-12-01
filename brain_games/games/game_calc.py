baad630b2b47cfef9ba9152ece15109ec5cdb783
procoview <simon10-00@mail.ru>
2023-12-01 23:59:57 +0300

    add do_calc function to game_calc.py

---
+++ brain_games/games/game_calc.py
@@ -0,0 +1,20 @@
+# file <game_calc.py>
+
+import random
+import operator
+
+question = "What is the result of the expression?"
+
+
+def do_calc():
+    global result
+    first_number = random.randint(0,100)
+    second_number = random.randint(0,100)
+    operations = { "+": operator.add, "-": operator.sub, "*": operator.mul }
+    random_operation = random.choice(list(operations.keys()))
+    result = str(operations[random_operation](first_number, second_number))
+    print(f'Question: {first_number} {random_operation} {second_number}')
+
+
+def main():
+    do_calc()
