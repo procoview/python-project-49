53f2cc769067430fd08f7112a4e2199981699a2c
procoview <simon10-00@mail.ru>
2023-12-01 23:03:14 +0300

    add do_even function to game_even.py

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -2,31 +2,20 @@
 
 from random import randint
 
-
-def even():
-    print("Welcome to the Brain Games!")
-    name = input("May I have your name? ")
-    print(f"Hello, {name}!")
-    print("Answer \"yes\" if the number is even, otherwise answer \"no\".")
-    i = 0
-    while i < 3:
-        random_number = randint(0, 100)
-        if (random_number % 2 == 0):
-            is_even = 'yes'
-        else:
-            is_even = 'no'
-        print(f'Question: {random_number}')
-        answer = input("Your answer: ")
-        if answer == is_even:
-            print("Correct!")
-            i += 1
-        else:
-            i = 0
-            print(f"'{answer}' is wrong answer ;(. "
-                  f"Correct answer was '{is_even}'.")
-            print(f"Let's try again, {name}!")
-    print(f"Congratulations, {name}!")
+question = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
 
-if __name__ == '__main__':
-    even()
+def do_even():
+    global result
+    random_number = randint(0,100)
+    if (random_number % 2 == 0):
+        result = 'yes'
+    else:
+        result = 'no'
+    print(f'Question: {random_number}')
+    return result
+
+        
+def main():
+    do_even()
+
