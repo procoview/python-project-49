fd6c76016371f1ef0ec6c8b557a6e58e3d6eb93b
procoview <simon10-00@mail.ru>
2023-12-06 23:08:24 +0300

    fix brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # file <brain_even.py>
 
-from brain_games.engine import *
+import brain_games.engine
 import brain_games.games.game_even
 
 
 def main():
-    launch_game(brain_games.games.game_even)
+    brain_games.engine.launch_game(brain_games.games.game_even)
 
 
 if __name__ == '__main__':
