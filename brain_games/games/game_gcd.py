2db2df1e58d33c6b9efc9ffd3dfe5fa9d3526cbd
procoview <simon10-00@mail.ru>
2023-12-11 23:13:09 +0300

    fix game_gcd.py

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,4 +1,4 @@
-#<file game_gcd.py>
+# file <game_gcd.py>
 
 from random import randint
 
@@ -7,8 +7,8 @@ question = "Find the greatest common divisor of given numbers."
 
 def do_gcd():
     global result
-    rand_number1 = randint(0,100)
-    rand_number2 = randint(0,100)
+    rand_number1 = randint(0, 100)
+    rand_number2 = randint(0, 100)
     while rand_number1 != 0 and rand_number2 != 0:
         if rand_number1 > rand_number2:
             rand_number1 = rand_number1 % rand_number2
