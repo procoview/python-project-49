33d1c44b751a7cfe204aa8c7b6357f1e35e1650c
procoview <simon10-00@mail.ru>
2023-12-11 23:04:57 +0300

    add do_gcd function to game_gcd.py

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,19 @@
+#<file game_gcd.py>
+
+from random import randint
+import math
+
+question = "Find the greatest common divisor of given numbers."
+
+
+def do_gcd():
+    global result
+    rand_number1 = randint(0,100)
+    rand_number2 = randint(0,100)
+    while rand_number1 != 0 and rand_number2 != 0:
+        if rand_number1 > rand_number2:
+            rand_number1 = rand_number1 % rand_number2
+        else:
+            rand_number2 = rand_number2 % rand_number1
+    result = str(rand_number1 + rand_number2)
+    print(f'Question: {rand_number1} {rand_number2}')
