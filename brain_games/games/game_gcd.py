4927d2a7c1f498e182465612e9a6acfc0817c2c7
procoview <simon10-00@mail.ru>
2023-12-12 22:50:42 +0300

    update game_gcd.py

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -11,7 +11,7 @@ def find_gcd(a, b):
     else:
         temp = a
     for i in range(1, temp + 1):
-        if((a % i == 0) and(b % i == 0)):
+        if ((a % i == 0) and (b % i == 0)):
             gcd = i
     return gcd
 
