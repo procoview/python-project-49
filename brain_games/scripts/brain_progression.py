b73f0351a8e40c9e895999627e640d463887a7c5
procoview <simon10-00@mail.ru>
2023-12-15 23:26:00 +0300

    update brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# file <brain_progression.py>
+
+import brain_games.engine
+import brain_games.games.game_progression
+
+
+def main():
+    brain_games.engine.launch_game(brain_games.games.game_progression)
+
+
+if __name__ == '__main__':
+    main()
