c6c28a4891d9b6213580ebd0465e179c493d96cc
procoview <simon10-00@mail.ru>
2023-12-15 23:18:47 +0300

    add do_progression function to game_progression.py

---
+++ brain_games/games/game_progression.py
@@ -0,0 +1,26 @@
+# file <game_progression.py>
+
+from random import randint
+
+question = "What number is missing in the progression?"
+
+
+def do_progression():
+    global result
+    num = randint(1, 50)
+    nums = []
+    for i in range(0, 10):
+        nums.append(num)
+        num += 2
+    index = randint(0, 9)
+    result = str(nums[index])
+    print(f'Question: ', end="")
+    for i in range(0, 10):
+        if i == index:
+            print('..', end=" ")
+        else:
+            print(nums[i], end=" ")
+
+
+def main():
+    do_progression()
