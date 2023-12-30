d5f2719bd19ad6134fcdec95ce83f6977e0e306b
procoview <simon10-00@mail.ru>
2023-12-30 20:23:56 +0300

    update game_prime.py

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -8,14 +8,15 @@ question = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
 def do_prime():
     global result
     random_number = randint(0, 100)
-    k=0
+    k = 0
     for i in range(2, random_number):
         if (random_number % i == 0):
-            k = k+1
+            k = k + 1
     if (k <= 0):
         result = "yes"
     else:
         result = "no"
+    print(f'Question: {random_number}')
     return result
 
 
