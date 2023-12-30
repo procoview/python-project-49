2deda5e20a3f90ec55e4fe0f1b12a46aff385253
procoview <simon10-00@mail.ru>
2023-12-30 20:12:28 +0300

    update brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# file <brain_prime.py>
+
+import brain_games.engine
+import brain_games.games.game_prime
+
+
+def main():
+    brain_games.engine.launch_game(brain_games.games.game_prime)
+
+
+if __name__ == '__main__':
+    main()
