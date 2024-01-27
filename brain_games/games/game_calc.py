f38bbafe5686d7fd23c51bdd32c177713de62367
procoview <simon10-00@mail.ru>
2024-01-27 22:25:35 +0300

    fix game_calc.py

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -6,8 +6,7 @@ import operator
 QUESTION = "What is the result of the expression?"
 
 
-def do_calc():
-    global result
+def main():
     first_number = random.randint(0, 100)
     second_number = random.randint(0, 100)
     operations = {"+": operator.add, "-": operator.sub, "*": operator.mul}
@@ -15,7 +14,3 @@ def do_calc():
     result = str(operations[random_operation](first_number, second_number))
     print(f'Question: {first_number} {random_operation} {second_number}')
     return result
-
-
-def main():
-    do_calc()
