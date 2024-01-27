e635d51536f0f44d1e15ab0e904e2b90094c5e51
procoview <simon10-00@mail.ru>
2024-01-27 22:26:20 +0300

    game_gcd.py

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -6,14 +6,9 @@ import math
 QUESTION = "Find the greatest common divisor of given numbers."
 
 
-def do_gcd():
-    global result
+def main():
     rand_number1 = randint(0, 100)
     rand_number2 = randint(0, 100)
     result = str(math.gcd(rand_number1, rand_number2))
     print(f'Question: {rand_number1} {rand_number2}')
     return result
-
-
-def main():
-    do_gcd()
