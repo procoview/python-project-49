e33dbf39542423373232cb0184e24a472af482f4
procoview <simon10-00@mail.ru>
2024-02-20 23:38:34 +0300

    add gcd.py

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,14 @@
+# file <game_gcd.py>
+
+from random import randint
+import math
+
+QUESTION = "Find the greatest common divisor of given numbers."
+
+
+def main():
+    rand_number1 = randint(0, 100)
+    rand_number2 = randint(0, 100)
+    result = str(math.gcd(rand_number1, rand_number2))
+    print(f'Question: {rand_number1} {rand_number2}')
+    return result
