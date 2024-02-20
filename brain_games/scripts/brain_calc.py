fd7f88b9e20ad8f2268d7574b9dcec1447c5be6b
procoview <simon10-00@mail.ru>
2024-02-20 23:41:31 +0300

    update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # file <brain_calc.py>
 import brain_games.engine
-import brain_games.games.game_calc
+import brain_games.games.calc
 
 
 def main():
-    brain_games.engine.launch_game(brain_games.games.game_calc)
+    brain_games.engine.launch_game(brain_games.games.calc)
 
 
 if __name__ == '__main__':
