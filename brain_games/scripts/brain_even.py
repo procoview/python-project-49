c2cd6f058c6198bffb47769a7f1a7f2e7ca42413
procoview <simon10-00@mail.ru>
2024-02-20 23:41:48 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,11 +2,11 @@
 # file <brain_even.py>
 
 import brain_games.engine
-import brain_games.games.game_even
+import brain_games.games.even
 
 
 def main():
-    brain_games.engine.launch_game(brain_games.games.game_even)
+    brain_games.engine.launch_game(brain_games.games.even)
 
 
 if __name__ == '__main__':
