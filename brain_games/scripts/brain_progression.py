265f4cc3050840d47e57f4d55e6f54b2907b97d8
procoview <simon10-00@mail.ru>
2024-02-20 23:42:40 +0300

    update brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -2,11 +2,11 @@
 # file <brain_progression.py>
 
 import brain_games.engine
-import brain_games.games.game_progression
+import brain_games.games.progression
 
 
 def main():
-    brain_games.engine.launch_game(brain_games.games.game_progression)
+    brain_games.engine.launch_game(brain_games.games.progression)
 
 
 if __name__ == '__main__':
