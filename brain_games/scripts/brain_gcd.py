cace005c37e553c4e2326fd1cbfdd5d865c8ee26
procoview <simon10-00@mail.ru>
2024-02-20 23:42:06 +0300

    update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -2,11 +2,11 @@
 # file <brain_gcd.py>
 
 import brain_games.engine
-import brain_games.games.game_gcd
+import brain_games.games.gcd
 
 
 def main():
-    brain_games.engine.launch_game(brain_games.games.game_gcd)
+    brain_games.engine.launch_game(brain_games.games.gcd)
 
 
 if __name__ == '__main__':
