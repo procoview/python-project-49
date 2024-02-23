9b09970aac17abf13153b7577ec6105a215a5335
procoview <simon10-00@mail.ru>
2024-02-23 16:35:08 +0300

    update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,6 +2,8 @@
 
 import prompt
 
+COUNTER = 3
+
 def launch_game(game):
     print("Welcome to the Brain Games!")
 
@@ -13,14 +15,12 @@ def launch_game(game):
 
     print(question)
 
-    for i in range(0, 3):
+    for i in range(0, COUNTER):
         result = game.main()
         answer = prompt.string("Your answer: ")
         if answer == result:
             print("Correct!")
-            i += 1
         else:
-            i = 0
             print(f"'{answer}' is wrong answer ;(. ", end='')
             print(f"Correct answer was '{result}'.")
             print(f"Let's try again, {name}!")
