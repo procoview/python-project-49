dd754df96e9a3d18ae01c9714d01d3304e2315f7
procoview <simon10-00@mail.ru>
2024-02-23 17:10:31 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,17 +5,21 @@ from random import randint
 QUESTION = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
 
 
-def main():
-    random_number = randint(0, 100)
+def is_prime(number):
     k = 0
-    for i in range(2, random_number):
-        if (random_number % i == 0):
+    for i in range(2, number):
+        if (number % i == 0):
             k = k + 1
     if (k <= 0):
-        result = "yes"
+        return "yes"
+    elif number == 0 or number == 1:
+        return "no"
     else:
-        result = "no"
-    if random_number == 0 or random_number == 1:
-        result = "no"
+        return "no"
+
+
+def main():
+    random_number = randint(0, 100)
+    result = is_prime(random_number)
     print(f'Question: {random_number}')
     return result
