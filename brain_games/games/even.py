b776fff2a7d720d23477f2ff14a28e0a5596ec20
procoview <simon10-00@mail.ru>
2024-02-23 17:02:30 +0300

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,9 +7,6 @@ QUESTION = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
 def main():
     random_number = randint(0, 100)
-    if (random_number % 2 == 0):
-        result = 'yes'
-    else:
-        result = 'no'
+    result = 'yes' if random_number % 2 == 0 else 'no'
     print(f'Question: {random_number}')
     return result
