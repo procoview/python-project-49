8bcc5c1dfaa503b78c8f1fa184e4f0714e7f1967
procoview <simon10-00@mail.ru>
2024-02-23 18:09:55 +0300

    update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,18 +5,18 @@ from random import randint
 QUESTION = "What number is missing in the progression?"
 
 
+def generate_progression(start, end, step):
+    numbers = list(range(start, end, step))
+    return numbers
+
+
 def main():
-    num = randint(1, 50)
-    nums = []
-    for i in range(0, 10):
-        nums.append(num)
-        num += 2
+    start_num = randint(1, 10)
+    end_num = randint(20, 30)
+    step_size = 2
+    progression = generate_progression(start_num, end_num, step_size)
     index = randint(0, 9)
-    result = str(nums[index])
-    print('Question: ', end="")
-    for i in range(0, 10):
-        if i == index:
-            print('..', end=" ")
-        else:
-            print(nums[i], end=" ")
+    result = str(progression[index])
+    str_progression = ', '.join(progression)
+    print(f'Question: {str_progression.replace(result, '..')}')
     return result
