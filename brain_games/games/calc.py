b384e7adb3833c3eca9a525dc0981b2eaca126bc
procoview <simon10-00@mail.ru>
2024-02-23 16:57:15 +0300

    update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,25 +1,20 @@
 # file <game_calc.py>
 
 import random
+import operator
 
 QUESTION = "What is the result of the expression?"
 
 
 def is_calc(first_number, random_operation, second_number):
-    if random_operation == "+":
-        answer = first_number + second_number
-    elif random_operation == "-":
-        answer = first_number - second_number
-    else:
-        answer = first_number * second_number
-    return answer
+    return random_operation[1](first_number, second_number)
 
 
 def main():
     first_number = random.randint(0, 100)
     second_number = random.randint(0, 100)
-    operations = ['+', '-', '*']
+    operations = [['+', operator.add], ['-', operator.sub], ['*', operator.mul]]
     random_operation = random.choice(operations)
     result = str(is_calc(first_number, random_operation, second_number))
-    print(f'Question: {first_number} {random_operation} {second_number}')
+    print(f'Question: {first_number} {random_operation[0]} {second_number}')
     return result
