7aaef4c9bd9c2386d177512f4315a52b616e04ce
procoview <simon10-00@mail.ru>
2024-03-04 22:02:16 +0300

    update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,15 +6,11 @@ import operator
 QUESTION = "What is the result of the expression?"
 
 
-def is_calc(first_number, random_operation, second_number):
-    return random_operation[1](first_number, second_number)
-
-
 def main():
     first_number = random.randint(0, 100)
     second_number = random.randint(0, 100)
     operations = [['+', operator.add], ['-', operator.sub], ['*', operator.mul]]
-    random_operation = random.choice(operations)
-    result = str(is_calc(first_number, random_operation, second_number))
-    question = f'{first_number} {random_operation[0]} {second_number}'
+    operation_sign, operation = random.choice(operations)
+    result = str(operation(first_number, second_number))
+    question = f'{first_number} {operation_sign} {second_number}'
     return result, question
