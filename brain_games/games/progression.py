4b809d7ddf386947bcf49772a7188dab7206d0d7
procoview <simon10-00@mail.ru>
2024-03-05 22:41:15 +0300

    update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,9 +6,9 @@ QUESTION = "What number is missing in the progression?"
 
 
 def main():
-    length = random.randint(5, 10)  
-    start = random.randint(1, 10)  
-    diff = random.randint(1, 5)  
+    length = random.randint(5, 10)
+    start = random.randint(1, 10)
+    diff = random.randint(1, 5)
     progression = list(range(start, start + diff * length, diff))
     random_index = random.randint(0, len(progression) - 1)
     result = str(progression[random_index])
