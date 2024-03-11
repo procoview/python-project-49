e1e346bb05c2c448162b88c4c3c5e3d666271645
procoview <simon10-00@mail.ru>
2024-03-11 22:14:35 +0300

    update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,7 @@ from random import randint
 QUESTION = "Answer \"yes\" if the number is even, otherwise answer \"no\"."
 
 
-def main():
+def generate_game():
     question = randint(0, 100)
     result = 'yes' if question % 2 == 0 else 'no'
     return result, question
